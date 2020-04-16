2ec74d4bbb326078333b233e075426be077aca9b
David Hersey <david@leapforward.us>
2020-04-16 15:16:32 -0400

    returning initiative count for need areas

--- app/models/need_area.rb
+++ app/models/need_area.rb
@@ -2,4 +2,9 @@ class NeedArea < ApplicationRecord
   has_and_belongs_to_many :need_categories
   has_many :needs, through: :need_categories
   has_many :initiatives, through: :needs
+
+
+  def initiative_count
+    initiatives.count
+  end  
 end
